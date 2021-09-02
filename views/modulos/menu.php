a484d3731ebf2a513ade4f54b5b98ca191e2ba68
paul1-7 <moicoxoxo@gmail.com>
2021-09-02 12:19:12 -0400

    navbar y menus completos

---
+++ views/modulos/menu.php
@@ -0,0 +1,135 @@
+<nav class="pcoded-navbar">
+<div class="pcoded-inner-navbar main-menu">
+    <div class="pcoded-navigatio-lavel">Menu</div>
+    <ul class="pcoded-item pcoded-left-item">
+        <li class="">
+            <a href="javascript:void(0)">
+                <span class="pcoded-micon"><i class="feather icon-home"></i></span>
+                <span class="pcoded-mtext">Dashboard</span>
+            </a>
+        </li>
+        
+        <li class="pcoded-hasmenu">
+            <a href="javascript:void(0)">
+                <span class="pcoded-micon"><i class="feather icon-settings"></i></span>
+                <span class="pcoded-mtext">Administracion General</span>
+            </a>
+            <ul class="pcoded-submenu">
+                
+                <li class=" ">
+                    <a href="menu-bottom.htm">
+                        <span class="pcoded-mtext">Datos del negocio</span>
+                    </a>
+                </li>
+                <li class=" ">
+                    <a href="box-layout.htm" target="_blank">
+                        <span class="pcoded-mtext">Datos de dosificación de facturas</span>
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li class="">
+            <a href="navbar-light.htm">
+                <span class="pcoded-micon"><i class="feather icon-package"></i></span>
+                <span class="pcoded-mtext">Categorias</span>
+            </a>
+        </li>
+        <li class="">
+            <a href="navbar-light.htm">
+                <span class="pcoded-micon"><i class="feather icon-users"></i></span>
+                <span class="pcoded-mtext">Clientes</span>
+            </a>
+        </li>
+        <li class="">
+            <a href="navbar-light.htm">
+                <span class="pcoded-micon"><i class="feather icon-shopping-cart"></i></span>
+                <span class="pcoded-mtext">Compras</span>
+            </a>
+        </li>
+        <li class="">
+            <a href="navbar-light.htm">
+                <span class="pcoded-micon"><i class="feather icon-list"></i></span>
+                <span class="pcoded-mtext">Marcas</span>
+            </a>
+        </li>
+        <li class="">
+            <a href="navbar-light.htm">
+                <span class="pcoded-micon"><i class="feather icon-bookmark"></i></span>
+                <span class="pcoded-mtext">Pedidos</span>
+            </a>
+        </li>
+        <li class="pcoded-hasmenu">
+            <a href="javascript:void(0)">
+                <span class="pcoded-micon"><i class="feather icon-layers"></i></span>
+                <span class="pcoded-mtext">Productos</span>
+            </a>
+            <ul class="pcoded-submenu">
+                <li class=" ">
+                    <a href="widget-statistic.htm">
+                        <span class="pcoded-mtext">Administrar Productos</span>
+                    </a>
+                </li>
+                <li class=" ">
+                    <a href="widget-data.htm">
+                        <span class="pcoded-mtext">Stock minimo</span>
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li class="">
+            <a href="navbar-light.htm">
+                <span class="pcoded-micon"><i class="feather icon-users"></i></span>
+                <span class="pcoded-mtext">Proveedores</span>
+            </a>
+        </li>
+        <li class="pcoded-hasmenu">
+            <a href="javascript:void(0)">
+                <span class="pcoded-micon"><i class="feather icon-file"></i></span>
+                <span class="pcoded-mtext">Reportes</span>
+            </a>
+            <ul class="pcoded-submenu">
+                <li class=" ">
+                    <a href="widget-statistic.htm">
+                        <span class="pcoded-mtext">Reportes de venta</span>
+                    </a>
+                </li>
+                <li class=" ">
+                    <a href="widget-data.htm">
+                        <span class="pcoded-mtext">Reportes de compra</span>
+                    </a>
+                </li>
+                <li class=" ">
+                    <a href="widget-data.htm">
+                        <span class="pcoded-mtext">Reportes de inventario</span>
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li class="">
+            <a href="navbar-light.htm">
+                <span class="pcoded-micon"><i class="feather icon-briefcase"></i></span>
+                <span class="pcoded-mtext">Reservas</span>
+            </a>
+        </li>
+        <li class="">
+            <a href="navbar-light.htm">
+                <span class="pcoded-micon"><i class="feather icon-star"></i></span>
+                <span class="pcoded-mtext">Roles</span>
+            </a>
+        </li>
+        <li class="">
+            <a href="navbar-light.htm">
+                <span class="pcoded-micon"><i class="feather icon-users"></i></span>
+                <span class="pcoded-mtext">Usuarios</span>
+            </a>
+        </li>
+        <li class="">
+            <a href="navbar-light.htm">
+                <span class="pcoded-micon"><i class="feather icon-shopping-cart"></i></span>
+                <span class="pcoded-mtext">Ventas</span>
+            </a>
+        </li>
+    </ul>
+
+</div>
+</nav>
---
+++ views/modulos/navbar.php
@@ -0,0 +1,48 @@
+<nav class="navbar header-navbar pcoded-header">
+                <div class="navbar-wrapper">
+
+                    <div class="navbar-logo">
+                        <div>
+                            <a class="mobile-menu " id="mobile-collapse" href="#!">
+                                <i class="feather icon-menu"></i>
+                            </a>
+                        </div>
+                        <div>
+                        <a href="index-1.htm">
+                            <img class="img-fluid" src="views\assets\images\logo.png" alt="Theme-Logo">
+                        </a>
+                        </div>
+                        
+                    </div>
+
+                    <div class="navbar-container container-fluid">
+                        
+                        <ul class="nav-right">
+                            <li class="user-profile header-notification">
+                                <div class="dropdown-primary dropdown">
+                                    <div class="dropdown-toggle" data-toggle="dropdown">
+                                        <img src="views\assets\images\avatar-4.jpg" class="img-radius" alt="User-Profile-Image">
+                                        <span>John Doe</span>
+                                        <i class="feather icon-chevron-down"></i>
+                                    </div>
+                                    <ul class="show-notification profile-notification dropdown-menu" data-dropdown-in="fadeIn" data-dropdown-out="fadeOut">
+                                        
+                                        <li>
+                                            <a href="user-profile.htm">
+                                                <i class="feather icon-user"></i> Profile
+                                            </a>
+                                        </li>
+                                        
+                                        <li>
+                                            <a href="auth-normal-sign-in.htm">
+                                                <i class="feather icon-log-out"></i> Logout
+                                            </a>
+                                        </li>
+                                    </ul>
+
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
